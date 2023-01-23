dcbc1fb6cccba3b37cc511228b8c349ad6b46cfc
Sergio Pieshko <sergio.pieshko@icloud.com>
2023-01-23 16:48:11 +0200

    card render

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -41,6 +41,27 @@ fetchTrending();
 function renderTrending(trends) {
   const markup = trends
     .map(trend => {
+      const genres = {
+        28: 'Action',
+        12: 'Adventure',
+        16: 'Animation',
+        35: 'Comedy',
+        80: 'Crime',
+        99: 'Documentary',
+        18: 'Drama',
+        10751: 'Family',
+        14: 'Fantasy',
+        36: 'History',
+        27: 'Horror',
+        10402: 'Music',
+        9648: 'Mystery',
+        10749: 'Romance',
+        878: 'Science Fiction',
+        10770: 'TV Movie',
+        53: 'Thriller',
+        10752: 'War',
+        37: 'Western',
+      };
       const { original_title, genre_ids, release_date } = trend;
       return ` <li class="card__list--item">
     <div class="card__wrapper">
@@ -48,9 +69,9 @@ function renderTrending(trends) {
         trend.poster_path
       }" alt="poster" />
       <h3 class="card__title">${original_title}</h3>
-      <p class="card__genre"><span>${genre_ids.join(
-        ' '
-      )}</span> | <span>${release_date}</span></p>
+      <p class="card__genre"><span>${genre_ids
+        .map(genre_id => genres[genre_id])
+        .slice(0, 1)}</span> | <span>${release_date.slice(0, 4)}</span></p>
     </div>
   </li>`;
     })
