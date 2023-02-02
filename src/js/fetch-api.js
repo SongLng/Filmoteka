57f5c0e1e682ed964c13dbbd8c0acafb9b063536
Sergio Pieshko <sergio.pieshko@icloud.com>
2023-02-02 13:08:04 +0200

    js pag

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -4,7 +4,7 @@ const API_KEY = '8a851f7b54845db9a7afea3bd46f2426';
 
 let pageNumber = 1;
 
-function fetchTrending() {
+export function fetchTrending() {
   fetch(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=en-US&page=${pageNumber}`
   )
@@ -20,34 +20,3 @@ function fetchTrending() {
 }
 
 fetchTrending();
-
-const btnLeftEl = document.querySelector('.btn__left');
-const btnRightEl = document.querySelector('.btn__right');
-const paginationList = document.querySelector('.pagination__list');
-let paginationNumber = 5;
-
-btnRightEl.addEventListener('click', paginationRight);
-btnLeftEl.addEventListener('click', paginationLeft);
-
-function paginationRight() {
-  const pagButtons = document.createElement('li');
-  pagButtons.classList.add('btn');
-
-  pagButtons.textContent = paginationNumber += 1;
-  paginationList.appendChild(pagButtons);
-  paginationList.firstElementChild.remove();
-  pageNumber += 1;
-
-  return fetchTrending();
-}
-
-function paginationLeft() {
-  const pagButtons = document.createElement('li');
-  pagButtons.classList.add('btn');
-  pagButtons.textContent = paginationNumber;
-  paginationList.appendChild(pagButtons);
-  paginationList.lastElementChild.remove();
-  pageNumber -= 1;
-
-  return fetchTrending();
-}
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,1 +1,65 @@
+import { fetchTrending } from './fetch-api';
+const btnLeftEl = document.querySelector('.btn__left');
 
+const btnRightEl = document.querySelector('.btn__right');
+const ulEl = document.querySelector('.pagination__list');
+
+let allPages = 15;
+
+function elem(allPages, page) {
+  let li = '';
+
+  let beforePages = page - 1;
+  let afterPages = page + 3;
+  let liActive;
+
+  if (page > 1) {
+    li += ` <li class="btn__left" onclick="elem(allPages,${page - 1})">
+    <span class="btn__left">
+      <img class="btn__img" src="./images/arrow-left.png" alt=""
+    /></span>
+  </li>`;
+  }
+  for (let pageLength = beforePages; pageLength <= afterPages; pageLength++) {
+    if (page === pageLength) {
+      liActive = 'active';
+    } else {
+      liActive = '';
+    }
+    li += `<li class="numb ${liActive}"><span>${pageLength}</span></li>`;
+  }
+  if (page < allPages) {
+    li += `  <li class="btn__right" onclick="elem(allPages,${page + 1})">
+    <span>
+      <img class="btn__img" src="./images/arrow-right.png" alt=""
+    /></span>
+  </li>`;
+  }
+  ulEl.innerHTML = li;
+}
+
+// btnLeftEl.addEventListener('click', elem(allPages, page - 1));
+elem(allPages, 2);
+
+// function paginationRight() {
+//   const pagButtons = document.createElement('li');
+//   pagButtons.classList.add('btn');
+
+//   pagButtons.textContent = paginationNumber += 1;
+//   paginationList.appendChild(pagButtons);
+//   paginationList.firstElementChild.remove();
+//   pageNumber += 1;
+
+//   return fetchTrending();
+// }
+
+// function paginationLeft() {
+//   const pagButtons = document.createElement('li');
+//   pagButtons.classList.add('btn');
+//   pagButtons.textContent = paginationNumber;
+//   paginationList.appendChild(pagButtons);
+//   paginationList.lastElementChild.remove();
+//   pageNumber -= 1;
+
+//   return fetchTrending();
+// }
