221224d1e68919d802f3a7e42206fc08f8c97cab
Sergio Pieshko <sergio.pieshko@icloud.com>
2023-02-02 15:17:37 +0200

    pagination

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -2,9 +2,9 @@ import renderTrending from './card-render';
 
 const API_KEY = '8a851f7b54845db9a7afea3bd46f2426';
 
-let pageNumber = 1;
+// let pageNumber = 1;
 
-export function fetchTrending() {
+export function fetchTrending(pageNumber = 1) {
   fetch(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=en-US&page=${pageNumber}`
   )
@@ -20,3 +20,5 @@ export function fetchTrending() {
 }
 
 fetchTrending();
+
+// const ul = document.querySelector('.pagination__list');
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,44 +1,72 @@
 import { fetchTrending } from './fetch-api';
-const btnLeftEl = document.querySelector('.btn__left');
 
-const btnRightEl = document.querySelector('.btn__right');
 const ulEl = document.querySelector('.pagination__list');
 
 let allPages = 15;
+let pageNumber = +1;
 
 function elem(allPages, page) {
-  let li = '';
-
   let beforePages = page - 1;
   let afterPages = page + 3;
   let liActive;
 
   if (page > 1) {
-    li += ` <li class="btn__left" onclick="elem(allPages,${page - 1})">
-    <span class="btn__left">
-      <img class="btn__img" src="./images/arrow-left.png" alt=""
-    /></span>
-  </li>`;
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    li.classList.add('btn__left');
+    span.innerHTML = `  <img class="btn__img" src="./images/arrow-left.png" alt=""
+    />`;
+    li.appendChild(span);
+    li.addEventListener('click', () => {
+      if (pageNumber === 1) {
+        fetchTrending();
+      } else fetchTrending((pageNumber = pageNumber - 1));
+    });
+    ulEl.appendChild(li);
   }
   for (let pageLength = beforePages; pageLength <= afterPages; pageLength++) {
+    if (pageLength > allPages) {
+      continue;
+    }
+    if (pageLength == 0) {
+      pageLength = pageLength + 1;
+    }
+
     if (page === pageLength) {
-      liActive = 'active';
+      liActive = 'btn__active';
     } else {
       liActive = '';
     }
-    li += `<li class="numb ${liActive}"><span>${pageLength}</span></li>`;
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    li.classList.add('numb');
+    span.innerHTML = ` ${pageLength}`;
+    li.appendChild(span);
+    li.addEventListener('click', () => {
+      if (pageLength === pageNumber) {
+        li.classList.add('btn__active');
+        fetchTrending((pageNumber = pageLength));
+      } else {
+        li.classList.remove('btn__active');
+      }
+    });
+
+    ulEl.appendChild(li);
   }
   if (page < allPages) {
-    li += `  <li class="btn__right" onclick="elem(allPages,${page + 1})">
-    <span>
-      <img class="btn__img" src="./images/arrow-right.png" alt=""
-    /></span>
-  </li>`;
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    li.classList.add('btn__right');
+    span.innerHTML = `  <img class="btn__img" src="./src/images/arrow-right.png" alt=""
+    />`;
+    li.appendChild(span);
+    li.addEventListener('click', () => {
+      fetchTrending((pageNumber = pageNumber + 1));
+    });
+    ulEl.appendChild(li);
   }
-  ulEl.innerHTML = li;
 }
 
-// btnLeftEl.addEventListener('click', elem(allPages, page - 1));
 elem(allPages, 2);
 
 // function paginationRight() {
